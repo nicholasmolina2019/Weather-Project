78f13c00eca0d4f9fbf8846888e1c8a1928941ae
Nick Molina <Nicholasmolina15@gmail.com>
2022-05-10 14:28:07 -0500

    finding the easiest way to just display api info

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-//created a object for storing functions and variables using the api
+$(document).ready(function(){
+ $('submit').click(function(){
+ 
 
-const weather = {
 
-  apiKey: "c8ca4c1b15df1fdb6434fbb8de36fa07"
-}
+
+ })
+})
+
+
+
+
