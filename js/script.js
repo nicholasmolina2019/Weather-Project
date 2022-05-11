604ea0a3b3b5426f0920023f53930ec779a3e59e
Nick Molina <Nicholasmolina15@gmail.com>
2022-05-11 17:46:30 -0500

    added ajax request

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-$(document).ready(function(){
-$(".submit").click(function(){
-//declaring variable
- let city = $(".search-city").val();
+ $(document).ready(function(){
+  $(".submit").click(function(){
+ //declaring variable
+  const city = $(".search-city").val();
+  if(city!= ' '){
+    $.ajax({
+     URL:'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=c8ca4c1b15df1fdb6434fbb8de36fa07&unios=imperial',
+     //https://openweathermap.org/current#data
+     //https://seir-batch.netlify.app/frontend-fundamentals/week-2/day-5/lecture-materials/intro-to-ajax-and-javascript-promises/
+     //https://api.jquery.com/jquery.ajax/
+     dataType: "jsonP"
+
+    
+    })
+   }
 
 
 
-
-})
-
-})
+   })
+ })
