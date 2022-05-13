f9d4a794c274dc13137c4e36e56364068d95adff
Nick Molina <Nicholasmolina15@gmail.com>
2022-05-12 21:08:12 -0500

    added final functionality for data to display when searching for city and fixed the measurements of getting temp

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
    $(document).ready(function(){
-     $(".submit").click(function() {
-    //declaring variable
-   const city = $(".search-city").val();
-    if(city!= ''){
-      $.ajax({
-      url:'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=c8ca4c1b15df1fdb6434fbb8de36fa07&units=imperial',
-       //https://openweathermap.org/current#data
-       //https://seir-batch.netlify.app/frontend-fundamentals/week-2/day-5/lecture-materials/intro-to-ajax-and-javascript-promises/
-       //https://api.jquery.com/jquery.ajax/
-       type:"GET",
-       dataType: "jsonp",
-       success:function(data){
-       const weather=show(data);
-       $(".temperature").html(weather);
+     $(".submit").click(() => {
+         //declaring variable
+         const city = $(".search-city").val();
+         if (city != '') {
+           $.ajax({
+             url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=c8ca4c1b15df1fdb6434fbb8de36fa07&units=imperial',
+             //https://openweathermap.org/current#data
+             //https://seir-batch.netlify.app/frontend-fundamentals/week-2/day-5/lecture-materials/intro-to-ajax-and-javascript-promises/
+             //https://api.jquery.com/jquery.ajax/
+             type: "GET",
+             dataType: "jsonp",
+             success: function (data) {
+               const weather = show(data);
+               $(".temperature").html(weather);
 
-      }
-
-    });
-  }
-}
+             }
+           });
+         }
+       }
 )
    });
 
