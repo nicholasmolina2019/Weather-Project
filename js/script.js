ce1103fe33d43fe149b827e680cd8ea9c0229732
Nick Molina <Nicholasmolina15@gmail.com>
2022-05-12 21:37:41 -0500

    fixed the url wouldnt display data when on the site

--- js/script.js
+++ js/script.js
@@ -4,7 +4,13 @@
          const city = $(".search-city").val();
          if (city != '') {
            $.ajax({
-             url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=c8ca4c1b15df1fdb6434fbb8de36fa07&units=imperial',
+             _url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c8ca4c1b15df1fdb6434fbb8de36fa07&units=imperial`,
+             get url() {
+               return this._url;
+             },
+             set url(value) {
+               this._url = value;
+             },
              //https://openweathermap.org/current#data
              //https://seir-batch.netlify.app/frontend-fundamentals/week-2/day-5/lecture-materials/intro-to-ajax-and-javascript-promises/
              //https://api.jquery.com/jquery.ajax/
